d4d239683b29bb99274d7ff8bd0743c1b5c4ddf6
KEVIN CORPUZ BOLADO <166228974+Drwillieong@users.noreply.github.com>
2025-09-29 16:38:04 +0800

    wow

--- backend/routes/analytics_fixed.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getAnalyticsData } from '../controllers/analyticsController_fixed.js';
-
-const router = express.Router();
-
-router.get('/', getAnalyticsData);
-
-export default router;
